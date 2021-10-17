1efe591022beab745f596fd69455eb1ac58da9a5
Gustavo G Passos <girardi_gustavo@outlook.com>
2021-10-17 14:39:57 -0300

    Commit 2
    Ajuste de nomes e criação de rota para verificar o progresso

--- src/index.js
+++ src/index.js
@@ -21,37 +21,32 @@ function checkUserExists(req,res,next){
   if(!user){
     return res.status(404).json({message:"User not found."});
   }
-
   req.user = user;
-
   next();
 }
 
 /**
  * rota para criação de um novo usuario
- * cada usuario tem:
- *  id
- *  nome
- *  username
+ * essa rota recebe:
+ *  nome > body;
+ *  username > body;
+ * 
+ * essa rota retorna:
+ *  o usuário que foi criado
  */
 app.post('/users', (req,res)=>{
   const {name, username} = req.body;
-
   const userAlreadyExists = users.find((user)=>user.username===username);
-
   if(userAlreadyExists){
     return res.status(400).json({message:"User already exists"});
   }
-
   const user = {
     id:uuidv4(),
     name,
     username,
     series: []
   }
-
   users.push(user)
-
   return res.status(201).send(user);
 });
 
@@ -64,9 +59,7 @@ app.post('/users', (req,res)=>{
 app.put('/users', checkUserExists, (req,res)=>{
   const {user} = req;
   const {name} = req.body;
-
   user.name = name;
-
   return res.status(201).send(user);
 });
 
@@ -75,40 +68,37 @@ app.put('/users', checkUserExists, (req,res)=>{
  * cada série tem:
  *  id
  *  nome
- *  quantidade de capítulos
- *  lista de capítulos
+ *  quantidade de episódios
+ *  lista de episódios
  */
 app.post('/series', checkUserExists,(req,res)=>{
   const {series} = req.user;
-  const {name, qt_chapters} = req.body;
-
-  const chapters = [];
-
-  for(let i=1;i<=qt_chapters;i++){
+  const {name, qt_episodes} = req.body;
+  const episodes = [];
+  for(let i=1;i<=qt_episodes;i++){
     const chap = {
-      chapter:i,
+      number:i,
       watched:false
     }
-    chapters.push(chap);
+    episodes.push(chap);
   }
-
   const serie = {
     id:uuidv4(),
     name,
-    qt_chapters,
-    chapters,
+    qt_episodes,
+    episodes,
   }
-
   series.push(serie);
-
   return res.status(201).send(serie);
-  
 });
 
 /**
  * rota para obter a lista de séries do usuário
  * essa rota recebe:
  *  username > header;
+ * 
+ * essa rota retorna:
+ *  a lista de series do usuário
  */
 app.get('/series', checkUserExists, (req,res)=>{
   const {series} = req.user;
@@ -121,32 +111,48 @@ app.get('/series', checkUserExists, (req,res)=>{
  *  id da serie > route param;
  *  username > header;
  *  número do capítulo > body;
+ * 
+ * essa rota retorna:
+ *  os dados da série que foi modificada;
  */
 app.patch('/series/:id/watched', checkUserExists, (req,res)=>{
   const {series} = req.user;
-  const {id_chapter} = req.body;
+  const {ep_number} = req.body;
   const {id} = req.params;
-
   const serie = series.find((serie)=>serie.id===id);
-
   if(!serie){
     return res.status(404).json({message:"Serie not found"});
   }
-
-  const {chapters} = serie;
-
-  const chapter = chapters.find((part)=>part.chapter===id_chapter);
-
-  if(!chapter){
-    return res.status(404).json({message:"Invalid chapter"});
+  const {episodes} = serie;
+  const episode = episodes.find((episode)=>episode.number===ep_number);
+  if(!episode){
+    return res.status(404).json({message:"Invalid episode"});
   }
-
-  chapter.watched = true;
-
+  episode.watched = true;
   return res.status(201).json(serie);
 });
 
-
+/**
+ * rota para obter o progresso de episódios assistidos
+ * essa rota recebe:
+ *  id da serie > route param;
+ *  username > header;
+ * 
+ * essa rota retorna:
+ *  o valor calculado do percentual de progresso sem decimais;
+ */
+app.get('/series/:id/progress', checkUserExists,(req,res)=>{
+  const {series} = req.user;
+  const {id} = req.params;
+  const serie = series.find((serie)=>serie.id===id);
+  if(!serie){
+    return res.status(404).json({message:"Serie not found"});
+  }
+  const {episodes} = serie;
+  const watched = episodes.filter((episode)=>episode.watched===true);
+  const progress = ((watched.length * 100)/serie.qt_episodes).toFixed(0);
+  return res.status(200).send(progress);
+});
 
 
 module.exports = app;
